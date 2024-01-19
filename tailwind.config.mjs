b8a600560ec59f603256abdbd9a722e333a6242d
Sergio Beleña <sergio.belena@wunderdog.fi>
2024-01-19 14:17:16 +0000

    Add Lint and prettier configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      animation: {
+        "border-width": "border-width 3s infinite alternate",
+      },
+      keyframes: {
+        "border-width": {
+          from: {
+            width: "10px",
+            opacity: "0",
+          },
+          to: {
+            width: "200px",
+            opacity: "1",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
